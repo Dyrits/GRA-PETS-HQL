05e730ebccaa2ab87d2810df8612845d428ea577
Dyrits <dyrits@outlook.com>
2022-07-01 16:45:04 -0500

    Initialization.

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,21 +1,53 @@
-import React, {useState} from 'react'
-import gql from 'graphql-tag'
-import { useQuery, useMutation } from '@apollo/react-hooks'
-import PetsList from '../components/PetsList'
-import NewPetModal from '../components/NewPetModal'
-import Loader from '../components/Loader'
+import React, {useState} from "react";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+
+import PetsList from "../components/PetsList";
+import NewPetModal from "../components/NewPetModal";
+import Loader from "../components/Loader";
+import {MutationTuple} from "@apollo/react-hooks/lib/types";
+
+const PETS = gql`
+  query Pets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`;
+
+const ADD_PET = gql`
+  mutation AddPet($input: NewPetInput!) {
+    addPet(input: $input) {
+      id
+      name
+      type
+      img
+    }
+  }
+`;
+
 
 
 export default function Pets () {
-  const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false);
+  const queryResult = useQuery(PETS);
+  const { pets } = queryResult.data;
+  const [addPet, mutationResult] = useMutation(ADD_PET);
 
-
-  const onSubmit = input => {
-    setModal(false)
+  const onSubmit = async input => {
+    setModal(false);
+    await addPet({ variables: {input} });
+    await queryResult.refetch();
   }
+
+  if (queryResult.loading || mutationResult.loading) { return <Loader />; }
+  if (queryResult.error || mutationResult.error) { return <p>An error occurred!</p>; }
   
   if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
+    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
   }
 
   return (
@@ -32,8 +64,8 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={pets} />
       </section>
     </div>
-  )
+  );
 }
